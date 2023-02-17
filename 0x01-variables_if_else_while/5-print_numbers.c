f5ca01cd77f78da1c8deea1f6871d703bb1abac7
Khaybee <kachieosuji11@gmail.com>
2023-02-17 10:39:15 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	putchar('\n');
+	}
+	return (0);
+}
