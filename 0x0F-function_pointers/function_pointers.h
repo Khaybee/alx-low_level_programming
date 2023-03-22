ba56a667ff4c1f1196fc4b2a113b893152d7670d
Khaybee <kachieosuji11@gmail.com>
2023-03-22 16:41:24 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,21 +1,12 @@
-#ifndef _HEADER_
-#define _HEADEER
-#include <stdlib.h>_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
-
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-int op_add(int a, int b);
-
-int op_sub(int a, int b);
-
-int op_mul(int a, int b);
-
-int op_div(int a, int b);
-
-int op_mod(int a, int
-
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
