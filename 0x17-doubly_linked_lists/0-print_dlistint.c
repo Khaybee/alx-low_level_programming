fb733b95d5aba5dcaf4ac926aa33685451ba090e
Khaybee <kachieosuji11@gmail.com>
2023-06-15 12:52:22 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t iterate = 0;
+	const dlistint_t *now = h;
+
+	while (now != NULL)
+	{
+		printf("%d\n", now->n);
+		iterate++;
+		now = now->next;
+	}
+
+	return iterate;
+}
