7f0a5b208f7c4abc79c9af80fdb5f34185df4908
Khaybee <kachieosuji11@gmail.com>
2023-06-15 19:02:48 -0700

    Palindrome

---
+++ 0x17-doubly_linked_lists/ok.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int is_palindrome(int num) {
+    int reversed = 0;
+    int original = num;
+
+    while (num != 0) {
+        int remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+
+void save_result_to_file(int result) {
+    FILE *file = fopen("102-result", "w");
+    if (file != NULL) {
+        fprintf(file, "%d", result);
+        fclose(file);
+    } else {
+        printf("Unable to open file for writing.\n");
+    }
+}
+
+int main() {
+    int largest_palindrome = 0;
+
+    for (int i = 100; i < 1000; i++) {
+        for (int j = i; j < 1000; j++) {
+            int product = i * j;
+            if (is_palindrome(product) && product > largest_palindrome) {
+                largest_palindrome = product;
+            }
+        }
+    }
+
+    save_result_to_file(largest_palindrome);
+
+    return 0;
+}
+
